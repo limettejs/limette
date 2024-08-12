6021d9eae88d89f57be52d735ea57bb1b32c8e0e
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2024-08-13 00:20:00 +0300

    feat: update pipeline

--- init/src/mod.ts
+++ init/src/mod.ts
@@ -1,7 +1,7 @@
 import { ensureDir } from "@std/fs/ensure-dir";
 
 // This value is changed in the release pipeline
-const LIMETTE_VERSION = "0.0.6";
+const LIMETTE_VERSION = "0.0.7";
 
 const projectName = prompt("Your project name?");
 
