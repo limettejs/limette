3f611df84471baba3312e686cd5959afe3ce9372
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2024-12-04 00:17:23 +0200

    feat: make light dom default for server components

--- src/server/rendering/limette-element-renderer.ts
+++ src/server/rendering/limette-element-renderer.ts
@@ -11,6 +11,8 @@ interface LimetteElement extends LitElement {
   __ctx?: ComponentContext; // Define the custom property here }
 }
 
+type LmtShadowRootMode = "open" | "closed" | "disabled";
+
 export const LimetteElementRenderer = (
   route: BuildRoute,
   componentContext: ComponentContext
@@ -32,7 +34,7 @@ export const LimetteElementRenderer = (
     override renderShadow(renderInfo: RenderInfo): RenderResult {
       const ctor = this.element.constructor as typeof LitElement & {
         __tailwind: boolean;
-        lightDom: boolean;
+        disableLightDom: boolean;
       };
 
       // We check if the element is inside of an is-land element
@@ -42,8 +44,8 @@ export const LimetteElementRenderer = (
         this.element.hasAttribute("island");
 
       // Islands are CSR'ed, so we can't render them in light DOM
-      if (ctor?.lightDom && !isIsland) {
-        this.shadowRootOptions.mode = "disabled";
+      if (ctor?.disableLightDom !== true && !isIsland) {
+        (this.shadowRootOptions.mode as LmtShadowRootMode) = "disabled";
       } else {
         this.shadowRootOptions.mode = "open";
       }
